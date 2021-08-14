7e9a1012c3db8fa7d205b11c142bbeafe7d31a84
unknown <dbar1090@gmail.com>
2021-08-14 08:09:44 -0700

    Added Interface

--- ng-demo-app/src/app/services/data.service.ts
+++ ng-demo-app/src/app/services/data.service.ts
@@ -1,33 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-
-export interface Chemicals {
-  position?: any;
-  name?: number;
-  weight?: boolean;
-  symbol: string;
-  username: string;
-  userId: string;
-  // data?: any;
-  // token: any;
-}
-
+import { IChemicals } from '../interfaces/IChemicals';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-  
+
   constructor(private http: HttpClient) { }
-  
-  // *********************************************************************** */
-  // * For Local Development - Set By Pass to TRUE to disable authentication  
-  // *********************************************************************** */
-  getAll(): Observable<any> {
-    return this.http.get("../../assets/data.json");    
+
+  getAll(): Observable<IChemicals[]> {
+    return this.http.get<IChemicals[]>("../../assets/data.json");
   }
-
-
 
 }
